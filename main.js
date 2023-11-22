936b1fdd58d8ff0c5a6c7ed026e2bc0c9ea53801
matheusvigilato <54921621+matheusvigilato@users.noreply.github.com>
2023-11-22 12:24:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 function playAudio(id_do_som) {
-    alert(id_do_som);
+
+    const audio = document.getElementById(id_do_som);
+
+    if(audio){
+        audio.play();
+    }else{
+        alert("Não foi possível reproduzir o som!");
+    }
+    
 }
